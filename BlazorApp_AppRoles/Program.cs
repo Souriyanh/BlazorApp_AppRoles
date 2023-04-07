461062d6ecd691f11b8c87a94127dbae6f6ab4dc
AzureAD\SVongsiprasom <souriyanh@peopleit.com>
2023-04-07 12:39:37 -0400

    All set on Graph Client Ext code fix. Next up , use graph to get calendar

--- BlazorApp_AppRoles/Program.cs
+++ BlazorApp_AppRoles/Program.cs
@@ -1,6 +1,7 @@
 using BlazorApp_AppRoles;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using BlazorApp_AppRoles.Graph;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
@@ -8,12 +9,14 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
-builder.Services.AddMicrosoftGraphClient("https://graph.microsoft.com/User.Read");
+//builder.Services.AddMicrosoftGraphClient("https://graph.microsoft.com/User.Read");
 
 builder.Services.AddMsalAuthentication(options =>
 {
     builder.Configuration.Bind("AzureAd", options.ProviderOptions.Authentication);
     options.ProviderOptions.DefaultAccessTokenScopes.Add("https://graph.microsoft.com/User.Read");
 });
+
+builder.Services.AddScoped<GraphClientFactory>();
 
 await builder.Build().RunAsync();
